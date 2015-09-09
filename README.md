24f578afe249d37efc091be799280274297305de
Nico F <nico@MacBook-Pro-de-Nico.local>
2015-09-09 02:48:34 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#Javascript
+
+##Tuto
+Ejercicios en javascript
+
+##Node
+Ejercicio desarrollados en Node.js
---
+++ manual/for-loop.js
@@ -0,0 +1,8 @@
+var total = 0;
+var limit = 10;
+
+for (i = total; i < limit; i++){
+  total += i;
+}
+
+console.log(total);
